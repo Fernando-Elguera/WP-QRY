4a2b2ec6db06d077a454f33e77880c7217f1b6d6
Fernando-Elguera <94646328+Fernando-Elguera@users.noreply.github.com>
2021-11-22 09:16:14 -0600

    Update appcsv.py

--- appcsv.py
+++ appcsv.py
@@ -7,7 +7,7 @@ import datetime
 import base64
 import numpy as np
 import os
-from pyxlsb import open_workbook as open_xlsb
+# from pyxlsb import open_workbook as open_xlsb
 import streamlit.components.v1 as components
 
 

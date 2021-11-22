3fd70f8e3543292e516f34cf0124d07eb1ecd62e
Fernando-Elguera <94646328+Fernando-Elguera@users.noreply.github.com>
2021-11-22 12:43:15 -0600

    Update appcsv.py

--- appcsv.py
+++ appcsv.py
@@ -113,8 +113,8 @@ total_sales = int(filtered_df1[mask]["W/O WIP Amount"].sum())
 
 # left_column, middle_column, right_column = st.columns(3)
 # with left_column:
-st.subheader("Total W/O WIP Amount:")
-st.subheader(f"MX $ {total_sales:,}")
+st.subheader("Total W/O WIP Amount:"  f"MX $ {total_sales:,}")
+# st.subheader(f"MX $ {total_sales:,}")
 # with middle_column:
 #     st.subheader("Average Rating:")
 #     st.subheader(f"{average_rating} {star_rating}")
